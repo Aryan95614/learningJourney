ab1a090af41a3132d13f271bf87260dfa9325cb4
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-09-19 20:20:16 -0400

    Updating Flask File and taking out the class

--- main.py
+++ main.py
@@ -4,32 +4,20 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 
-class Backend:
 
-    def __init__(self):
-        pass
 
-    def start_app(self):
-        """
-        Start the App
-        """
-        if __name__ == "__main__":
-            app.run(debug=True)
 
-    @staticmethod
-    @app.route('/')
-    def main_route():
-        return 'Hello World'
+@app.route('/')
+def main_route():
+    return 'Hello World'
 
-    @staticmethod
-    @app.route('/api/data', methods=['GET'])
-    @cross_origin()
-    def data_route():
-        print("there was a connection")
-        data = {"status": "success", "message": "It works?"}
-        return jsonify(data)
 
-backend = Backend()
+@app.route('/api/data', methods=['GET'])
+@cross_origin()
+def data_route():
+    print("there was a connection")
+    data = {"status": "success", "message": "It works?"}
+    return jsonify(data)
 
 if __name__ == "__main__":
-    backend.start_app()
+    app.run(debug=True)
